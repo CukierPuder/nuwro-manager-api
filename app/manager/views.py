1d28d4ba90afb237f7fd96c66f7bec694891d2ad
Lukasz Nowak <cukier_puder@hotmail.com>
2019-07-23 19:00:45 +0200

    Re-factor Experiment, Measurement and Nuwroversion viewsets

--- app/manager/views.py
+++ app/manager/views.py
@@ -6,43 +6,31 @@ from core.models import Experiment, Measurement, Nuwroversion
 from manager import serializers
 
 
-class ExperimentViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin,
-                        mixins.CreateModelMixin):
+class BaseFileAttrViewSet(viewsets.GenericViewSet,
+                          mixins.ListModelMixin,
+                          mixins.CreateModelMixin):
+    """Base viewset for DataFile and ResultFile"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Return the list of all objects ordered by name"""
+        return self.queryset.order_by('-name')
+
+
+class ExperimentViewSet(BaseFileAttrViewSet):
     """Manage experiments in database"""
     queryset = Experiment.objects.all()
     serializer_class = serializers.ExperimentSerializer
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
-
-    def get_queryset(self):
-        """Return the list of all objects"""
-        return self.queryset.order_by('-name')
 
 
-class MeasurementViewSet(viewsets.GenericViewSet,
-                         mixins.ListModelMixin,
-                         mixins.CreateModelMixin):
+class MeasurementViewSet(BaseFileAttrViewSet):
     """Manage measurements in database"""
     queryset = Measurement.objects.all()
     serializer_class = serializers.MeasurementSerializer
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
-
-    def get_queryset(self):
-        """Return the list of all objects ordered by name"""
-        return self.queryset.order_by('-name')
 
 
-class NuwroversionViewSet(viewsets.GenericViewSet,
-                          mixins.ListModelMixin,
-                          mixins.CreateModelMixin):
+class NuwroversionViewSet(BaseFileAttrViewSet):
     """Manage nuwroversions in database"""
     queryset = Nuwroversion.objects.all()
     serializer_class = serializers.NuwroversionSerializer
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
-
-    def get_queryset(self):
-        """Return the list of all objects ordered by name"""
-        return self.queryset.order_by('-name')
